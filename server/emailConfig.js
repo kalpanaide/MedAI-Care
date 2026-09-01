f572eaba1d2b2edc07a69ce264cf027ad8b549f8
Kalpana <kalpanaide86@gmail.com>
2026-09-01 14:14:51 +0530

    Add timeout settings for email transporter

--- server/emailConfig.js
+++ server/emailConfig.js
@@ -6,7 +6,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 module.exports = transporter;
